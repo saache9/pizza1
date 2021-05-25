3df882636b8c55e1312b05bf683ee5ab1ceb6fa0
saache9 <84771428+saache9@users.noreply.github.com>
2021-05-25 15:25:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-menu_list_array = ["Chicken Tandoori Pizza","Veg Supreme Pizza","Paneer Tikka Pizza","Deluxe Veggie Pizza","Veg Extravaganza Pizza"];
+menu_list_array = ["Chicken Tandoori Pizza",
+"Veg Supreme Pizza",
+"Paneer Tikka Pizza",
+"Deluxe Veggie Pizza",
+"Veg Extravaganza Pizza"];
     
     function getmenu(){
         var htmldata;
